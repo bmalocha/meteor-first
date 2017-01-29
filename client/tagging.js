01b4f980b05f1d34334068e5dc90d68c0f76d5e1
Bartosz Malocha <Bartosz Malocha>
2017-01-29 21:04:29 +0100

    store all rules in one mongo document

--- client/tagging.js
+++ client/tagging.js
@@ -1,47 +1,71 @@
 import {
-  State
+    State
 } from './state.js';
 import {
-  TaggingRules
+    TaggingRules
 } from '../imports/api/taggingRules.js';
 
 Template.taggingRules.events({
-  'click button.addRule' (event, instance) {
-    var ruleName = $("input[name='ruleName']").val();
-    var tag = $("input[name='tag']").val();
-    console.log("add rule: " + ruleName + " " + tag + JSON.stringify(State.getFilters()));
+    'click button.addRule' (event, instance) {
+        var ruleName = $("input[name='ruleName']").val();
+        var tag = $("input[name='tag']").val();
+        console.log("add rule: " + ruleName + " " + tag + JSON.stringify(State.getFilters()));
 
-    var rule = {
-      tag: tag,
-      name: ruleName,
-      filters: State.getFilters()
-    };
+        var rule = {
+            _id: newId(),
+            tag: tag,
+            name: ruleName,
+            filters: State.getFilters()
+        };
 
-    TaggingRules.insert(rule);
+        var rules = TaggingRules.findOne();
+        if (rules) {
+            rules.rules.push(rule);
+            TaggingRules.update(rules._id, {
+                $set: {
+                    rules: rules.rules
+                }
+            });
+        } else {
+            TaggingRules.insert({
+                rules: [rule]
+            });
+        }
 
-    $("input[name='ruleName']").val('');
-    $("input[name='tag']").val('');
-  },
-  'click li' (event, instance) {
-    const id = event.target.dataset.id;
-    console.log("clicked li " + id);
+        $("input[name='ruleName']").val('');
+        $("input[name='tag']").val('');
+    },
 
-    const rule = TaggingRules.findOne({
-      _id: id
-    });
-    applyFilters(rule.filters);
-  }
+    'click li' (event, instance) {
+        const id = event.target.dataset.id;
+        console.log("clicked li " + id);
+        const rule = getRules().find(rule => rule._id == id);
+        if (rule) {
+            applyFilters(rule.filters);
+        }
+    }
 })
 
 Template.taggingRules.helpers({
-  rules() {
-    return TaggingRules.find({});
-  }
+    rules() {
+        return getRules();
+    }
 })
 
+function getRules() {
+    var rules = TaggingRules.findOne() || {
+        rules: []
+    };
+    return rules.rules;
+}
+
 function applyFilters(filters) {
-  _.keys(filters.textFilters).forEach(key => {
-    $("input[name='" + key + "']").val(filters.textFilters[key]);
-  })
-  State.setFilters(filters);
+    _.keys(filters.textFilters).forEach(key => {
+        $("input[name='" + key + "']").val(filters.textFilters[key]);
+    })
+    State.setFilters(filters);
+}
+
+function newId() {
+    return (new Mongo.ObjectID)._str;
 }
