809e8f93ea40643b58ce35e54c9856a95927efaf
Bartosz Malocha <Bartosz Malocha>
2017-05-09 19:40:37 +0200

    add missing tags

--- client/tagTree.js
+++ client/tagTree.js
@@ -2,40 +2,97 @@ import {
     TaggingRules
 } from '../imports/api/taggingRules.js';
 
+const ALL_NODE_ID = 1;
+const UNASSIGNED_NODE_ID = 2;
+
 Template.tagTree.onRendered(() => {
-    var data = [{
-            name: 'all',
-            children: [{
-                    name: 'child1'
-                },
-                {
-                    name: 'child2'
-                }
-            ]
-        },
-        {
-            name: 'node2',
-            children: [{
-                name: 'child3'
-            }]
-        }
-    ];
-    $('#tree1').tree({
-        data: data,
+    var treeData = loadTree();
+    var tree = $('#tree1');
+
+    tree.tree({
+        data: treeData,
         dragAndDrop: true,
         autoOpen: 0
     });
 
-    console.log("rules");
-    var rules = TaggingRules.findOne();
-    rules.rules.map(r=>r.tag).forEach(tag=>
-    $('#tree1').tree(
-        'appendNode', {
-            name: tag,
-            id: newId()
-        }
-    ));
+    addMissingTags(tree, treeData);
+    saveTree();
 });
+
+Template.tagTree.events({
+    'click button.addTag' (event, instance) {
+        var tag = $("input[name='tagGroup']").val();
+        var tree = $('#tree1');
+        appendNode(tree, tag, ALL_NODE_ID);
+        $("input[name='tagGroup']").val('');
+        saveTree();
+    }
+});
+
+
+
+function addMissingTags(tree, treeData) {
+    var tags = getAllTags();
+    crossOutExistingTags(tags, treeData);
+    tags.forEach(tag => appendNode(tree, tag, UNASSIGNED_NODE_ID));
+}
+
+function crossOutExistingTags(tags, treeData){
+    treeData.forEach(node => {
+        var i = tags.indexOf(node.name);
+        if(i!=-1){
+            tags.splice(i, 1);
+        }
+        if(typeof node.children !=='undefined'){
+            crossOutExistingTags(tags, node.children);
+        }
+    });
+}
+
+function appendNode(tree, name, parent_node_id) {
+    var parent_node = tree.tree('getNodeById', parent_node_id);
+    tree.tree(
+        'appendNode', {
+            name: name,
+            id: newId()
+        }, parent_node
+    )
+}
+
+function getAllTags() {
+    var rules = TaggingRules.findOne();
+    return rules.rules.map(r => r.tag);
+}
+
+function loadTree() {
+    var rules = TaggingRules.findOne();
+    var tree = rules.tree || baseTree();
+    return tree;
+}
+
+function saveTree() {
+    var tree = $('#tree1');
+    var treeData = JSON.parse(tree.tree('toJson'));
+
+    var rules = TaggingRules.findOne();
+    TaggingRules.update(rules._id, {
+        $set: {
+            tree: treeData
+        }
+    });
+}
+
+function baseTree() {
+    return [{
+            name: 'all',
+            id: ALL_NODE_ID
+        },
+        {
+            name: 'Unassigned',
+            id: UNASSIGNED_NODE_ID
+        }
+    ];
+}
 
 function newId() {
     return (new Mongo.ObjectID)._str;
