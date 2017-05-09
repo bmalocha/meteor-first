364a9e15d7346f0b9495eac14b394a245cae9c87
Bartosz Malocha <Bartosz Malocha>
2017-05-09 20:40:17 +0200

    filter by selected start

--- client/tagTree.js
+++ client/tagTree.js
@@ -5,20 +5,6 @@ import {
 const ALL_NODE_ID = 1;
 const UNASSIGNED_NODE_ID = 2;
 
-Template.tagTree.onRendered(() => {
-    var treeData = loadTree();
-    var tree = $('#tree1');
-
-    tree.tree({
-        data: treeData,
-        dragAndDrop: true,
-        autoOpen: 0
-    });
-
-    addMissingTags(tree, treeData);
-    saveTree();
-});
-
 Template.tagTree.events({
     'click button.addTag' (event, instance) {
         var tag = $("input[name='tagGroup']").val();
@@ -26,10 +12,38 @@ Template.tagTree.events({
         appendNode(tree, tag, ALL_NODE_ID);
         $("input[name='tagGroup']").val('');
         saveTree();
+    },
+    'click button.loadTree' (event, instance) {
+        var treeData = loadTree();
+        var tree = $('#tree1');
+
+        tree.tree({
+            data: treeData,
+            dragAndDrop: true,
+            autoOpen: 0
+        });
+
+        addMissingTags(tree, treeData);
+        saveTree();
+    },
+    'click button.saveTree' (event, instance) {
+        saveTree();
+    },
+    'click button.filterByGroup' (event, instance) {
+        var tree = $('#tree1');
+        var node = tree.tree('getSelectedNode');
+        var tags = flattenTags(node);
+        console.log("Selected ", node," flat:", tags);
     }
 });
 
-
+function flattenTags(node){
+    var result=[node.name];
+    node.children.forEach(child=>
+        result=result.concat(flattenTags(child))
+    );
+    return result;
+}
 
 function addMissingTags(tree, treeData) {
     var tags = getAllTags();
@@ -37,13 +51,13 @@ function addMissingTags(tree, treeData) {
     tags.forEach(tag => appendNode(tree, tag, UNASSIGNED_NODE_ID));
 }
 
-function crossOutExistingTags(tags, treeData){
+function crossOutExistingTags(tags, treeData) {
     treeData.forEach(node => {
         var i = tags.indexOf(node.name);
-        if(i!=-1){
+        if (i != -1) {
             tags.splice(i, 1);
         }
-        if(typeof node.children !=='undefined'){
+        if (typeof node.children !== 'undefined') {
             crossOutExistingTags(tags, node.children);
         }
     });
@@ -73,7 +87,7 @@ function loadTree() {
 function saveTree() {
     var tree = $('#tree1');
     var treeData = JSON.parse(tree.tree('toJson'));
-
+    console.log("Saved", treeData);
     var rules = TaggingRules.findOne();
     TaggingRules.update(rules._id, {
         $set: {
